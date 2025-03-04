85ccba74efaa6e4754cf60dc96d96f5cc8eeaea8
Hayle-HB <haylemeskelhaylemariam@gmail.com>
2025-03-04 04:30:00 +0300

    update rendering static files

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const connectDB = require("./config/mongoDB");  
+const connectDB = require("./config/mongoDB");   
 const express = require("express"); 
 const path = require("path");
 const cookieParser = require("cookie-parser");
